13757a2bc2aedaebcdfb9decdb654aa45c5c1b3b
Cornelius Soon <cornelius.soon@gmail.com>
2023-12-15 12:11:38 +1300

    Add webFormAddBook as htmlMixin module with htmlMixin closure

---
+++ src/javascript/htmlMixins/webFormAddBook.js
@@ -0,0 +1,23 @@
+const webFormAddBook = (() => {
+
+    const htmlMixin = () => {
+
+        const getAddBookButton = () => document.querySelector('#form-add-book-button')
+        const getInputBookTitle = () => document.querySelector('#input-book-title')
+        const getInputBookAuthors = () => document.querySelector('#input-book-authors')
+        const getInputBookPages = () => document.querySelector('#input-book-title')
+        const getInputBookIsRead = () => document.querySelector('#input-book-read')
+
+        return {
+            getAddBookButton,
+            getInputBookTitle,
+            getInputBookAuthors,
+            getInputBookPages,
+            getInputBookIsRead
+        }
+    }
+
+    return { htmlMixin }
+})();
+
+export default webFormAddBook
